1ade094a880a21cb5af826d4b8c60f7ea4f15ec7
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-08-07 08:22:04 -0300

    initial commit

---
+++ .github/pull_request_template.md
@@ -0,0 +1,9 @@
+<h1>Context</h1>
+    <p></p>
+<hr></hr>
+<h2>Changes</h2>
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    // import "../app.css" # In case we need custom css
+
+    // // Import the Analytics package, and the SvelteKit dev variable. // when we hook up to vercel later on for deployment
+    // import { dev } from '$app/environment'
+    // import { inject } from '@vercel/analytics'
+
+    // // Inject the Analytics functionality // vercel on prod
+    // inject({ mode: dev ? 'development' : 'production' })
+</script>
+  
+<slot /> <!--Like the Reacts equivalent of children-->
