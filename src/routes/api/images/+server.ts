6532fc091362143f460a036fc43f73a616d4a9d6
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-08-07 15:38:10 -0300

    chore: making request on server

--- src/routes/api/images/+server.ts
+++ src/routes/api/images/+server.ts
@@ -1,8 +1,12 @@
 // make the call to the API in here so it isn't accesible on the client
 
-export function GET() {
-    
+import { IMAGES_API } from '$env/static/private' // import from .env file
 
+
+export async function GET() {
+    // make the call to the API
+    const res = await fetch(IMAGES_API)
+    const simpson_images = await res.json()
     // needs to return a Response
-    return new Response(JSON.stringify({ message: 'This should be the data'}), { status: 200 })
+    return new Response(JSON.stringify(simpson_images), { status: 200 })
 }
