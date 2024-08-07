ed3b61384c1e9f12545a899c22ea58f6c7852cda
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-08-07 10:21:34 -0300

    fix: removing body inside main due to update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,32 +27,30 @@
 </script>
 
 <main>
-    <body>
-        <section>
-            <div>
-                <h1>Bienvenidos al traductor de frases-meme de <strong>Los Simpsons</strong></h1>
-                <h4>Te pasa que no viste <strong>Los Simpsons</strong> y no entendés las referencias cuando la gente las usa en el día a día?</h4>
-                <p>Bueno, además de que te invita a formar parte de un club 'no vi los Simpsons en mi infancia', acá te dejo un buscador de frases-meme donde, si hago bien mi trabajo, te explica el contexto.</p>
-            </div>
+    <section>
+        <div>
+            <h1>Bienvenidos al traductor de frases-meme de <strong>Los Simpsons</strong></h1>
+            <h4>Te pasa que no viste <strong>Los Simpsons</strong> y no entendés las referencias cuando la gente las usa en el día a día?</h4>
+            <p>Bueno, además de que te invita a formar parte de un club 'no vi los Simpsons en mi infancia', acá te dejo un buscador de frases-meme donde, si hago bien mi trabajo, te explica el contexto.</p>
+        </div>
 
-            <div>
-                <input 
-                    type='text'
-                    placeholder='Llamaré a los borrachos'
-                    name='phrase'
-                    on:input={(e) => searchPhrase(e)}                
-                >
+        <div>
+            <input 
+                type='text'
+                placeholder='Llamaré a los borrachos'
+                name='phrase'
+                on:input={(e) => searchPhrase(e)}                
+            >
 
-                {#each results as quotes, index}
-                    <ul>
-                        <li>
-                            <h3>{quotes.phrase}</h3>
-                            <p>{quotes.explanation}</p>
-                        </li>
-                    </ul>
-                    
-                {/each}
-            </div>
-        </section>
-    </body>
+            {#each results as quotes, index}
+                <ul>
+                    <li>
+                        <h3>{quotes.phrase}</h3>
+                        <p>{quotes.explanation}</p>
+                    </li>
+                </ul>
+                
+            {/each}
+        </div>
+    </section>
 </main>
