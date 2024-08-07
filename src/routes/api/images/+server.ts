f529176c773d93a3f81da5b1449c950df8f8cd1f
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-08-07 15:38:52 -0300

    styling: correcting comments

--- src/routes/api/images/+server.ts
+++ src/routes/api/images/+server.ts
@@ -4,9 +4,9 @@ import { IMAGES_API } from '$env/static/private' // import from .env file
 
 
 export async function GET() {
-    // make the call to the API
+    // make the call to the external API
     const res = await fetch(IMAGES_API)
     const simpson_images = await res.json()
-    // needs to return a Response
+    // return the Response passing the retrieved data
     return new Response(JSON.stringify(simpson_images), { status: 200 })
 }
