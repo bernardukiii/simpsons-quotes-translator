10570afa3d3457582b1bcbe6ab253fa736616bd0
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-08-07 09:01:58 -0300

    chore: adding title, subtitle & displaying the data on screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,29 @@
-<h1>Hello world! BDKIdev</h1>
+<script>
+    import data from '../data/simpsons-quotes.json'
+
+</script>
+
+<main>
+    <body>
+        <section>
+            <div>
+                <h1>Bienvenidos al traductor de frases-meme de <strong>Los Simpsons</strong></h1>
+                <h4>Te pasa que no viste <strong>Los Simpsons</strong> y no entendés las referencias cuando la gente las usa en el día a día?</h4>
+                <p>Bueno, además de que te invita a formar parte de un club 'no vi los Simpsons en mi infancia', acá te dejo un buscador de frases-meme donde, si hago bien mi trabajo, te explica el contexto.</p>
+            </div>
+
+            <div>
+                <!-- need an input field here -->
+                {#each data as quotes, index}
+                    <ul>
+                        <li>
+                            <h3>{quotes.phrase}</h3>
+                            <p>{quotes.explanation}</p>
+                        </li>
+                    </ul>
+                    
+                {/each}
+            </div>
+        </section>
+    </body>
+</main>
