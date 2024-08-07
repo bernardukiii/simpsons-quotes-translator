5ec6c2f82de1f0cee8cf3f4015856432a7248d70
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-08-07 15:23:51 -0300

    chore: setting up GET function and api directory

---
+++ src/routes/api/images/+server.ts
@@ -0,0 +1,8 @@
+// make the call to the API in here so it isn't accesible on the client
+
+export async function GET() {
+    const response = await fetch() 
+
+    // needs to return a Response
+    return new Response()
+}
