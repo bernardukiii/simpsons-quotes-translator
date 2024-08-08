8f33303cc07e71de33999bdf69172e54358c7e92
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-08-08 13:01:06 -0300

    chore: adding flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: 'selector',
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
